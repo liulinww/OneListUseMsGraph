58e46ce6ab485d79282e08a39dbdec203fe31a8d
liulinww <66872884+liulinww@users.noreply.github.com>
2020-06-24 10:14:51 +0800

    利用新的Graph1.0API获取

--- onedrive.py
+++ onedrive.py
@@ -20,17 +20,16 @@ class _ItemInfo:
 class OneDrive():
     _request_headers = {'User-Agent': 'ISV|MoeClub|OneList/1.0',
                         'Accept': 'application/json; odata.metadata=none'}
-
+    _request_headers={'Content-Type':'application/x-www-form-urlencoded'
+            }
     def __init__(self):
         self.api_url = ''
         self.resource_id = ''
-
-        self.expires_on = ''
         self.access_token = ''
         self.refresh_token = config.token
 
-    def get_access(self, resource='https://api.office.com/discovery/'):
-        res = self._http_request('https://login.microsoftonline.com/common/oauth2/token', method='POST', data={
+    def get_access(self, resource='https://graph.microsoft.com/v1.0/me/'):
+        res = self._http_request('https://login.microsoftonline.com/common/oauth2/v2.0/token', method='POST', data={
             'client_id': 'ea2b36f6-b8ad-40be-bc0f-e5e4a4a7d4fa',
             'client_secret': 'h27zG8pr8BNsLU0JbBh5AOznNS5Of5Y540l/koc7048=',
             'redirect_uri': 'http://localhost/onedrive-login',
@@ -39,7 +38,6 @@ class OneDrive():
             'resource': resource
         })
 
-        self.expires_on = res['expires_on']
         self.access_token = res['access_token']
         self.refresh_token = res['refresh_token']
 
@@ -48,20 +46,17 @@ class OneDrive():
             exit(1)
 
     def get_resource(self):
-        res = self._http_request(
-            'https://api.office.com/discovery/v2.0/me/services')
+        self.api_url = 'https://graph.microsoft.com/v1.0/me'
+        self.resource_id = 'https://graph.microsoft.com/v1.0/me/'
 
-        for item in res['value']:
-            if item['serviceApiVersion'] == 'v2.0':
-                self.api_url = item['serviceEndpointUri']
-                self.resource_id = item['serviceResourceId']
-
-        if not self.api_url:
-            raise Exception('Failed to get api url')
-
-    def list_items(self, path='/'):
-        url = '%s/drive/root:%s/?expand=children(select=name,size,file,folder,parentReference,lastModifiedDateTime)' % (
+    def list_items(self, path=''):
+        url = '%s/drive/root?$expand=children($select=name,size,file,folder,parentReference,lastModifiedDateTime)' % (
+            self.api_url)
+        if len(path)>1:
+            url = '%s/drive/root:/%s?$expand=children($select=name,size,file,folder,parentReference,lastModifiedDateTime)' % (
             self.api_url, parse.quote(path_format(path)))
+        
+        
         res = self._http_request(url)
 
         info = _ItemInfo()
